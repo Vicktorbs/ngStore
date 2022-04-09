8c6b3e487fe8f4c63f7b4505c29f696095d0eb65
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-04-09 15:56:06 -0500

    Countes. img component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Product } from '../app/models/product.models';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,35 +7,13 @@ import { Product } from '../app/models/product.models';
 })
 export class AppComponent {
   imgParent = ''
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/img/album.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/img/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/img/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/img/books.jpg'
-    },
-  ]
+  showImage = true;
 
   onLoaded(img: string) {
     console.log('log padre', img);
+  }
+
+  toggleImg() {
+    this.showImage = !this.showImage
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,50 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit {
 
-  @Input() img: string = '';
+  img: string = '';
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    // Code
+  }
+  @Input() alt: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/img/bike.jpg'
+  counter = 0;
+  counterFn: number | undefined;
 
-  constructor() { }
+  constructor() {
+    // Before render y no se deben usar operaciones asincronas - solo corre una vez
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // before - during render
+    // changes inputs -- multiples times ca vez que se actualice un input
+  }
 
   ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('running counter');
+    }, 1000)
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children -- once time
+  }
+
+  ngOnDestroy() {
+    // delete -- once time
+    window.clearInterval(this.counterFn)
   }
 
   imgError() {
