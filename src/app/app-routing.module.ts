b75151aa85105378e834a7a28278c3dac5d08470
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-05-02 20:20:04 -0500

    Preload - CustomStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CustomPreloadService } from "./services/custom-preload.service";
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
+    data: {
+      preload: true
+    }
   },
   {
     path: 'cms',
@@ -20,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: CustomPreloadService
   })],
   exports: [RouterModule]
 })
